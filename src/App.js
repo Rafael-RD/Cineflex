f5a2498078a7bcc6f31c2dadb76d1faff7f63e8d
Rafael <rafaelreinertd@hotmail.com>
2023-03-09 16:11:41 -0300

    Head

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import styled from "styled-components"
+import HomePage from "./pages/HomePage/HomePage"
+import SeatsPage from "./pages/SeatsPage/SeatsPage"
+import SessionsPage from "./pages/SessionsPage/SessionsPage"
+import SuccessPage from "./pages/SuccessPage/SuccessPage"
+
+export default function App() {
+    return (
+        <BrowserRouter>
+           <NavContainer>CINEFLEX</NavContainer>
+            <Routes>
+            <Route exact path="/" element={<HomePage />} />
+            <Route exact path="/" element={<SeatsPage />} />
+            <Route exact path="/" element={<SessionsPage />} />
+            <Route exact path="/" element={<SuccessPage />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+const NavContainer = styled.div`
+    width: 100%;
+    height: 70px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #C3CFD9;
+    color: #E8833A;
+    font-family: 'Roboto', sans-serif;
+    font-size: 34px;
+    position: fixed;
+    top: 0;
+    a {
+        text-decoration: none;
+        color: #E8833A;
+    }
+`
